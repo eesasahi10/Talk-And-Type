8937eaec0724893ff5947ad578c0945980bf06fa
eesasahi10 <eesasahi10@gmail.com>
2026-03-22 16:35:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 # It stores { "7-digit-code": "The spoken text" }
 mailboxes = {}
 
+current_code = ""
 @app.route('/')
 def index():
     # This serves your Talk&Type.html file from the templates folder
@@ -15,10 +16,23 @@ def index():
 # DOOR 1: Generates the 7-digit code for the website
 @app.route('/generate_code')
 def generate_code():
+    global current_code  # This tells Python to use the tank on Line 10
     new_code = str(random.randint(1000000, 9999999))
-    mailboxes[new_code] = None # Create an empty mailbox for this code
+    current_code = new_code # This saves the code for the phone to find later
+    mailboxes[new_code] = None
     print(f"New Room Created: {new_code}")
     return jsonify({"code": new_code})
+# Add this to your main.py
+@app.route('/connect', methods=['POST'])
+def connect():
+    data = request.json
+    user_code = data.get('code')
+    
+    # Check if the code the phone sent matches the one on the screen
+    if user_code == current_code:
+        return {"status": "success", "message": "Linked!"}, 200
+    else:
+        return {"status": "error", "message": "Invalid Code"}, 400
 
 # DOOR 2: The Phone calls this to "Drop Off" the text
 @app.route('/send_text', methods=['POST'])
